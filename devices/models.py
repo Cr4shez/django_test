0b21476eb34740ff0f9e78897b41ecf079de5e52
Crash <bandicoot191@gmail.com>
2021-08-18 18:34:21 +1000

    Model Added

--- devices/models.py
+++ devices/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Device(models.Model):
+    """Generic warning device - Siren or a Megaphone"""
+
+    SIREN = "SR"
+    MEGAPHONE = "MG"
+    TYPE_CHOICES = [(SIREN, "Сирена"),
+                    MEGAPHONE, "Громкоговоритель"]
+
+    name = models.CharField(
+        verbose_name='Название', max_length=200)
+    type = models.CharField(
+        verbose_name='Тип устройства',
+        max_length=2,
+        choices=TYPE_CHOICES,
+        default=SIREN)
+    address = models.CharField(
+        verbose_name='Адрес размещения', max_length=200)
+    latitude = models.FloatField(
+        verbose_name='Широта')
+    # DecimalField для указания шести знаков после запятой?
+    longitude = models.FloatField(
+        verbose_name='Долгота')
+    sound_reaching_radius = models.PositiveIntegerField(
+        verbose_name="Радиус зоны звукопокрытия (в метрах)")
